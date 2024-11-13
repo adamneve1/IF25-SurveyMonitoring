dc93f5be9c95efbef5518d6a5135889533ca442a
adamneve1 <ttrrrash@gmail.com>
2024-11-13 16:36:49 +0700

    fix error

--- database/migrations/2024_10_11_144010_manpower_dl.php
+++ database/migrations/2024_10_11_144010_manpower_dl.php
@@ -11,13 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('manpower_dls', function (Blueprint $table) {
+        $table->id(); // ID manpower
+        $table->string('nama');
         });
     }
 
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('manpowers');
     }
 };
