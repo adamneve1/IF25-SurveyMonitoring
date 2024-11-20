52b34c9d648f7e6e2fe21e1f6b7aa1b48c4f0cde
fazri yazid <fazrimyazid2@gmail.com>
2024-11-21 02:39:46 +0700

    Model manpower dls

---
+++ app/Models/Manpower_dls.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Manpower_dls extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+
+    // Tentukan nama tabel yang digunakan model ini
+    protected $table = 'manpower_idls';
+
+    // Tentukan kolom yang dapat diisi secara massal
+    protected $fillable = [
+        'nama'
+    ];
+
+    public function manhours()
+    {
+        return $this->hasMany(Manhour::class, 'proyek_id');
+    }
+}
