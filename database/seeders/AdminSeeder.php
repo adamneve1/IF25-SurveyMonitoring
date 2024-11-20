ce4c113d0c207da997fed0ed53265edd098b5a8d
fazri yazid <fazrimyazid2@gmail.com>
2024-11-21 02:15:35 +0700

    Seeder untuk admin

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    // jalankan command 'php artisan db:seed --class=UserSeeder'
+    public function run(): void
+    {
+        //Seeder admin email 'admin@lks.com' password: '1234'
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@lks.com',
+            'password' => Hash::make('1234'),
+            'remember_token' => null,
+            'created_at' => now(),
+            'updated_at' => now(),
+            'is_admin' => 0,
+        ]);
+    }
+}
