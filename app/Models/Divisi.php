d596a013304b41071d68170f3a1bf8e5369b6497
meqhh <fazrimyazid@gmail.com>
2025-03-10 21:42:21 +0700

    [UPDATE] Change divisi model

--- app/Models/Divisi.php
+++ app/Models/Divisi.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Divisi extends Model
 {
@@ -12,15 +14,15 @@ class Divisi extends Model
     public $table = 'divisi';
 
     protected $fillable = [
-      'name', 'reference'
+      'name'
     ];
 
-    public function manpower_idl()
+    public function manpower_idl():HasMany
     {
-        return $this->hasMany(Manpower_idl::class, 'proyek_id');
+        return $this->hasMany(Manpower_idl::class, 'divisi_id');
     }
-    public function manpower_dl()
+    public function manpower_dl(): HasMany
     {
-        return $this->hasMany(Manpower_dl::class, 'proyek_id');
+        return $this->hasMany(Manpower_dl::class, 'divisi_id');
     }
 }
