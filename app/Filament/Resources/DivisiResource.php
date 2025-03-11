16507888e48cd52400d3e10438120614fedeceaa
meqhh <fazrimyazid@gmail.com>
2025-03-11 13:00:46 +0700

    Add Divisi form

---
+++ app/Filament/Resources/DivisiResource.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\DivisiResource\Pages;
+use App\Filament\Resources\DivisiResource\RelationManagers;
+use App\Models\Divisi;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class DivisiResource extends Resource
+{
+    protected static ?string $model = Divisi::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+    protected static ?string $navigationLabel = 'Divisi';
+    protected static ?string $navigationGroup = 'Kelola';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->placeholder('e.g Welding')
+                    ->label('Divisi'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->label('Divisi'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDivisis::route('/'),
+            'create' => Pages\CreateDivisi::route('/create'),
+            'edit' => Pages\EditDivisi::route('/{record}/edit'),
+        ];
+    }
+}
